8719d351b6f80550ac72f6b664aee9aa2ee773e9
Karsten Taylor <karstentaylor0@gmail.com>
2020-07-07 13:42:00 -0700

    need genres

--- app.js
+++ app.js
@@ -5,32 +5,47 @@ const app = express();
 
 app.use(morgan('common'));
 
-app.listen(8000, () => {
-  console.log('Server started on PORT 8000');
-});
+function compareValues(key, order = 'asc') {
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
+    const varA = typeof a[key] === 'string' ? a[key].toUpperCase() : a[key];
+    const varB = typeof b[key] === 'string' ? b[key].toUpperCase() : b[key];
+    let comparison = 0;
+    if (varA > varB) comparison = 1;
+    if (varA < varB) comparison = -1;
+    return order === 'desc' ? comparison * -1 : comparison;
+  };
+}
 
 const apps = require('./playstore-data.js');
 
 app.get('/apps', (req, res) => {
-  const { search = '', sort } = req.query;
+  const { sort = '' } = req.query;
+  const { genre = '' } = req.query;
 
-  if (sort) {
-    if (!['rating', 'app'].includes(sort)) {
-      return res.status(400).send('Sort must be one of rating or app');
-    }
-  }
+  let results = apps;
+  const genres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
 
-  let results = apps.filter((apps) =>
-    apps.App.toLowerCase().includes(search.toLowerCase())
-  );
-
-  if (sort) {
-    results.sort((a, b) => {
-      return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+  if (genre && !genres.includes(genre.toLowerCase())) {
+    return res.status(400).send('Please select from available genres');
+  } else {
+    results = results.filter((results) => {
+      return results.Genres.toLowerCase().includes(genre.toLowerCase());
     });
   }
 
+  if (sort === 'rating') {
+    results = results.sort(compareValues('Rating'));
+  }
+  if (sort === 'app') {
+    results = results.sort(compareValues('App'));
+  }
+
   res.json(results);
+});
+
+app.listen(8000, () => {
+  console.log('Server started on PORT 8000');
 });
 
 // sort	'rating' or 'app'	sort the list by either rating or app, any other value results in an error, if no value provided do not perform a sort.
